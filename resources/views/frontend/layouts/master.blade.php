10da880a29c435777ac179ffa122d949960f6156
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2015-12-28 00:38:18 -0700

    Cache Fix :(

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -35,7 +35,7 @@
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="/frontend/css/logo-nav.css">
-    <link rel="stylesheet" href="/frontend/css/custom.css">
+    <link rel="stylesheet" href="/frontend/css/custom2.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
