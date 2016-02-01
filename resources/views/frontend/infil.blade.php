f3cbc8b2173c7a526373bca6965da19c1f4c9f99
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2016-01-31 22:36:57 -0700

    Formatting Changes to Infil News

--- resources/views/frontend/infil.blade.php
+++ resources/views/frontend/infil.blade.php
@@ -8,12 +8,14 @@
 
 @section('content')
     <div class="container">
-        <h1>1st Rapid Response Force - INFIL INTEL</h1>
+        <h1>1st Rapid Response Force</h1>
+        <h2>INFIL INTEL</h2>
         <hr>
         @foreach($articles as $infil)
             <h3>{{$infil->title}} <small>{{$infil->type}}</small></h3>
             <p>Written by {{$infil->vpf}} <small>{{$infil->publish_date->diffForHumans()}}</small></p>
             {!! $infil->body !!}
+            <hr>
         @endforeach
         <p>Make sure to report in!</p>
     </div>
