da239be022e7d0c96a46471bde0a2ad016bcdf0e
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2016-01-31 22:17:15 -0700

    Added credit to writer and date difference

--- resources/views/frontend/infil.blade.php
+++ resources/views/frontend/infil.blade.php
@@ -11,6 +11,7 @@
         <h1>1st RRF - INFIL INTEL</h1>
         @foreach($articles as $infil)
             <h2>{{$infil->title}} <smalL>{{$infil->type}}</smalL></h2>
+            <p>Written by {{$infil->vpf}} <small>{{$infil->publish_date->diffForHumans()}}</small></p>
             {!! $infil->body !!}
         @endforeach
         <p>Make sure to report in!</p>
