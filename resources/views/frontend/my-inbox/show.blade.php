8129c76a501f78757d10f600cadf664cae15412d
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2016-04-11 13:43:21 -0600

    Added last viewed to inbox

--- resources/views/frontend/my-inbox/show.blade.php
+++ resources/views/frontend/my-inbox/show.blade.php
@@ -29,6 +29,7 @@
             </div>
             <div class="media-body">
                 <h4 class="media-heading"><a href="/roster/{{$par->user->vpf->id}}">{{$par->user->vpf}}</a></h4>
+                <p><small>Last Viewed: {{$par->last_read->diffForHumans()}}</small></p>
             </div>
         </div>
     @endforeach
