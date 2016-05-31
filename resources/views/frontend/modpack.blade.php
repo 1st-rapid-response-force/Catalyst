67977e6be63d1093aebd05dcb9a6140ab033291b
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2016-05-31 09:30:44 -0600

    Update modpack.blade.php

--- resources/views/frontend/modpack.blade.php
+++ resources/views/frontend/modpack.blade.php
@@ -15,10 +15,11 @@
 @section('content')
     <div class="container">
         <h1>Modpack</h1>
-        <p>The 1st RRF Modpack consist of several mods complied to ensure the best simulation experience. You will need <a href="http://content.1st-rrf.com/infil/setup.exe">Infil - 1st RRF Official Launcher</a> in order to set up the repository. It is important to note that our modpack is publicly available and required for play on our public servers.</p>
+        <p>The 1st RRF Modpack consist of several mods complied to ensure the best simulation experience. You will need <a href="http://content.1st-rrf.com/infil/InfilModpackUpdater.zip">Infil - 1st RRF Official Modpack Updater</a> in order to set up the repository. It is important to note that our modpack is publicly available and required for play on our public servers.</p>
         <h2>Instructions on Downloading your mods with Infil</h2>
         <ol>
-            <li>Run the <a href="http://content.1st-rrf.com/infil/setup.exe">setup.exe</a> file</li>
+            <li>Download the <a href="http://content.1st-rrf.com/infil/InfilModpackUpdater.zip">InfilModpackUpdater.zip</a> file</li>
+            <li>Extract and Run infil_setup.application</li>
             <li>Setup your mod directory and ARMA 3 directory</li>
             <li>Press the Update Button or if you see none, validate your mods.</li>
             <li>Your Mods will autodownload, and once they are done, a play button will appear</li>
